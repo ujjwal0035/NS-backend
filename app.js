3b40b334bc55318fd2a1763809f125d8e1c9a787
Ujjwal <ujjwal0035@gmail.com>
2021-03-06 00:50:06 +0530

    Third day

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+const mongoose =require('mongoose');
 app.use(bodyParser.json());
+
+const DB_USER = "Ujju";
+const PASSWORD = encodeURIComponent('123');
+const DB_URL = `mongodb://${DB_USER}:${PASSWORD}@localhost:27017/newtonSchool`;
+
+mongoose.connect(DB_URL).then(()=>{
+    console.log("Dataconnection connected successfull");
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.get('/start',function(req,res){
     const responseObj={
